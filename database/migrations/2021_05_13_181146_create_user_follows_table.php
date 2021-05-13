212bb018a996eb270e7040710e09f8e1fa58d779
Killian Mahé <killianmahe.pro@gmail.com>
2021-05-13 20:13:42 +0200

    Create user follow migrations

---
+++ database/migrations/2021_05_13_181146_create_user_follows_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserFollowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_follows', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            // Follower
+            $table->unsignedBigInteger('follower_id');
+            $table->foreign('follower_id')->references('id')->on('users');
+
+            // Followed
+            $table->unsignedBigInteger('followed_id');
+            $table->foreign('followed_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_follows');
+    }
+}
