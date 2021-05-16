6f01dccc0c84c0794cef5dccccade6e4b3792cc4
Killian Mahé <killianmahe.pro@gmail.com>
2021-05-16 22:56:23 +0200

    Add markasread method

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Http\Resources\NotificationCollection;
 
@@ -11,15 +12,41 @@ class NotificationController extends Controller
      * Get the unread notifications of the authenticated user.
      *
      * @param Request $request
-     * @return mixed
+     * @return \NotificationCollection
      */
-    public function getUnread(Request $request): mixed
+    public function getUnread(Request $request): \NotificationCollection
     {
-        return $request->user()->unreadNotifications;
+        return new NotificationCollection($request->user()->unreadNotifications);
     }
 
-    public function get(Request $request)
+    /**
+     * Get all the notification of the authenticated user.
+     *
+     * @param Request $request
+     * @return NotificationCollection
+     */
+    public function get(Request $request): NotificationCollection
     {
         return new NotificationCollection($request->user()->notifications()->paginate(5));
+    }
+
+    /**
+     * Mark a notification as read.
+     *
+     * @param Request $request
+     * @param String $id
+     * @return JsonResponse
+     */
+    public function markAsRead(Request $request, String $id): JsonResponse
+    {
+        foreach ($request->user()->unreadNotifications as $notification) {
+            if ($notification->id == $id) {
+                $notification->markAsRead();
+
+                return new JsonResponse();
+            }
+        }
+
+        abort(404);
     }
 }
